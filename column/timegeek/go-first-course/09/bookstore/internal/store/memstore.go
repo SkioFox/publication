667e5b68b61e44cb552872dfd43cc407d9c1fc17
zouj01 <zouj01@mingyuanyun.com>
2024-05-13 14:30:05 +0800

    feat: update

--- column/timegeek/go-first-course/09/bookstore/internal/store/memstore.go
+++ column/timegeek/go-first-course/09/bookstore/internal/store/memstore.go
@@ -25,7 +25,13 @@ func (ms *MemStore) Create(book *mystore.Book) error {
 	if _, ok := ms.books[book.Id]; ok {
 		return mystore.ErrExist
 	}
+	/**
+	如果ms.books[book.id] = book，那么由于存储的value是一个指针，当外部的book发生变化时，map中存储的value实际上也会跟着变化。
+	我的代码中通过“clone”，实际上是将存储在map中的book与外部传入的book分离开来，它们是两块不同的存储区域。
 
+	简而言之，这段代码首先通过解引用得到了book指针所指向的结构体的一个副本（赋值给了nBook），然后将这个副本的地址存入了一个映射中，作为特定键（book.Id）对应的值。
+	这样的操作可能用在需要在不影响原结构体的同时，以某种方式修改或引用该结构体的一个副本的情景下，比如在实现深拷贝或者需要按ID管理结构体实例的场景。
+	*/
 	nBook := *book
 	ms.books[book.Id] = &nBook
 
@@ -91,9 +97,10 @@ func (ms *MemStore) Delete(id string) error {
 func (ms *MemStore) GetAll() ([]mystore.Book, error) {
 	ms.RLock()
 	defer ms.RUnlock()
-
+	// 注意，这里进行了深拷贝，确保返回的书籍切片中的每个书籍都是原始书籍的副本，外部对返回切片中的书籍所做的修改不会影响到MemStore内部的数据。
 	allBooks := make([]mystore.Book, 0, len(ms.books))
 	for _, book := range ms.books {
+
 		allBooks = append(allBooks, *book)
 	}
 	return allBooks, nil
--- column/timegeek/go-first-course/09/bookstore/store/store.go
+++ column/timegeek/go-first-course/09/bookstore/store/store.go
@@ -17,6 +17,11 @@ type Book struct {
 type Store interface {
 	Create(*Book) error
 	Update(*Book) error
+	/**
+	这两个方法的返回值为什么不是*Book呢？查询出来的结果应该是Book的实例了，返回值类型是Book会发生Copy么？
+	因为memstore底层使用的map类型为map[string]*mystore.Book， value用的是Book指针，如果直接返回指针有风险，这样外面的代码就可以直接修改memstore中的数据了。
+	返回Book类型时会有一次mem copy。
+	*/
 	Get(string) (Book, error)
 	GetAll() ([]Book, error)
 	Delete(string) error
